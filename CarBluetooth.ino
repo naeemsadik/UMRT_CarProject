eef228cd7e215f25bf60c8baae60262712b02fdc
naeemsadik <67340415+naeemsadik@users.noreply.github.com>
2023-10-01 22:52:11 +0600

    UpdatedIndentation

--- CarBluetooth.ino
+++ CarBluetooth.ino
@@ -1,22 +1,22 @@
 #include "BluetoothSerial.h"
 #include <NewPing.h>
 
-#define ENA 25  
-#define IN1 2 
-#define IN2 4 
-#define ENB 26  
-#define IN3 15  
-#define IN4 13  
+#define ENA 25
+#define IN1 2
+#define IN2 4
+#define ENB 26
+#define IN3 15
+#define IN4 13
 #define LED_RED 16
 #define LED_GREEN 17
 
-#define TRIGGER_PIN 5 
-#define ECHO_PIN 18   
-#define MAX_DISTANCE 200 
+#define TRIGGER_PIN 5
+#define ECHO_PIN 18
+#define MAX_DISTANCE 200
 
-const char *pin = "1234"; 
+const char *pin = "1234";
 
-NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE); 
+NewPing sonar(TRIGGER_PIN, ECHO_PIN, MAX_DISTANCE);
 
 String device_name = "TODOS";
 
@@ -32,13 +32,13 @@ BluetoothSerial SerialBT;
 
 void setup() {
   Serial.begin(9600);
-  SerialBT.begin(device_name); //Bluetooth device name
+  SerialBT.begin(device_name);  //Bluetooth device name
   Serial.printf("The device with name \"%s\" is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str());
-  //Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
-  #ifdef USE_PIN
-    SerialBT.setPin(pin);
-    Serial.println("Using PIN");
-  #endif
+//Serial.printf("The device with name \"%s\" and MAC address %s is started.\nNow you can pair it with Bluetooth!\n", device_name.c_str(), SerialBT.getMacString()); // Use this after the MAC method is implemented
+#ifdef USE_PIN
+  SerialBT.setPin(pin);
+  Serial.println("Using PIN");
+#endif
 
   // Motor control pins as outputs
   pinMode(ENA, OUTPUT);
@@ -47,8 +47,8 @@ void setup() {
   pinMode(ENB, OUTPUT);
   pinMode(IN3, OUTPUT);
   pinMode(IN4, OUTPUT);
-  pinMode(LED_RED,OUTPUT);
-  pinMode(LED_GREEN,OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
 
   // Set motor A and B to stop initially
   digitalWrite(IN1, LOW);
@@ -64,12 +64,12 @@ void forward() {
     stop();
     return;
   }
-  
+
   digitalWrite(IN1, HIGH);
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 255); // Adjust the speed as needed
+  analogWrite(ENA, 255);  // Adjust the speed as needed
   analogWrite(ENB, 255);
 }
 
@@ -78,7 +78,7 @@ void backward() {
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 255); // Adjust the speed as needed
+  analogWrite(ENA, 255);  // Adjust the speed as needed
   analogWrite(ENB, 255);
 }
 
@@ -87,7 +87,7 @@ void left() {
   digitalWrite(IN2, HIGH);
   digitalWrite(IN3, HIGH);
   digitalWrite(IN4, LOW);
-  analogWrite(ENA, 255); // Adjust the speed as needed
+  analogWrite(ENA, 255);  // Adjust the speed as needed
   analogWrite(ENB, 255);
 }
 
@@ -96,7 +96,7 @@ void right() {
   digitalWrite(IN2, LOW);
   digitalWrite(IN3, LOW);
   digitalWrite(IN4, HIGH);
-  analogWrite(ENA, 255); // Adjust the speed as needed
+  analogWrite(ENA, 255);  // Adjust the speed as needed
   analogWrite(ENB, 255);
 }
 
@@ -110,12 +110,12 @@ void stop() {
 }
 
 void loop() {
- if (SerialBT.available()> 0) {
+  if (SerialBT.available() > 0) {
 
-   digitalWrite(LED_GREEN, LOW);
-   digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_RED, HIGH);
 
-    char command = SerialBT.read(); // Read the incoming character
+    char command = SerialBT.read();  // Read the incoming character
 
     // Control the car based on the received command
     switch (command) {
@@ -129,7 +129,7 @@ void loop() {
         Serial.println("Backward");
         delay(100);
         break;
-      case 'L':        
+      case 'L':
         left();
         Serial.println("Left");
         delay(100);
@@ -146,6 +146,6 @@ void loop() {
       default:
         stop();
         break;
+    }
+  }
 }
- }
- }
